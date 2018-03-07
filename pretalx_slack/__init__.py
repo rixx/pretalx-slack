f36dc23f38df6781d684ba53314a80a9e4332a36
Tobias Kunze <rixx@cutebit.de>
2018-03-07 20:41:13 +0100

    Initial commit from cookiecutter

---
+++ pretalx_slack/__init__.py
@@ -0,0 +1,20 @@
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy
+
+
+class PluginApp(AppConfig):
+    name = 'pretalx_slack'
+    verbose_name = 'Slack integration for pretalx'
+
+    class PretalxPluginMeta:
+        name = ugettext_lazy('Slack integration for pretalx')
+        author = 'Tobias Kunze'
+        description = ugettext_lazy('Receive notifications whenever a submission changes its state.')
+        visible = True
+        version = '0.0.0'
+
+    def ready(self):
+        from . import signals  # NOQA
+
+
+default_app_config = 'pretalx_slack.PluginApp'
