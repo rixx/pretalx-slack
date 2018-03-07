5d2396bfe304456888b7d3d0f3d63cb14da85c3a
Tobias Kunze <rixx@cutebit.de>
2018-03-07 21:23:45 +0100

    Send message to hard-configured webhook

--- pretalx_slack/signals.py
+++ pretalx_slack/signals.py
@@ -1,8 +1,32 @@
+import json
+
+import requests
+from django.conf import settings
 from django.dispatch import receiver
+from django.utils.translation import ugettext_lazy as _
 
 from pretalx.submission.signals import submission_state_change
 
 
+webhook = settings.CONFIG.get('slack', 'webhook_url')
+
 @receiver(submission_state_change, dispatch_uid='pretalx_slack')
 def submission_state_change(sender, old_state, submission, user, **kwargs):
-    print(f'I see you went from {old_state} to {submission.state}, {user.name}')
+    if not webhook:
+        print('no webhook url available')
+        return
+
+    try:
+        text = _('Submission <{url}|»{title}«> has been changed from {old_state} to {new_state} by {name}.').format(
+            url=submission.orga_urls.base.full(),
+            title=submission.title,
+            old_state=old_state,
+            new_state=submission.state,
+            name=user.name,
+        )
+        response = requests.post(
+            webhook, headers={'Content-type': 'application/json'},
+            data=json.dumps({'text': text}),
+        )
+    except Exception as e:
+        print(e)
