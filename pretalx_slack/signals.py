e37f0912816ff92def306241f7a9f11c512d3fb0
Tobias Kunze <rixx@cutebit.de>
2018-03-07 20:56:26 +0100

    Handle state change

--- pretalx_slack/signals.py
+++ pretalx_slack/signals.py
@@ -1,1 +1,8 @@
-# Register your receivers here
+from django.dispatch import receiver
+
+from pretalx.submission.signals import submission_state_change
+
+
+@receiver(submission_state_change, dispatch_uid='pretalx_slack')
+def submission_state_change(sender, old_state, submission, user, **kwargs):
+    print(f'I see you went from {old_state} to {submission.state}, {user.name}')
