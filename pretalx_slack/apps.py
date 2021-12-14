2b9651f16a1ce94db9dab9c3de979f9d3ac7a5d6
Tobias Kunze <r@rixx.de>
2021-12-14 12:55:56 +0100

    Update code style

--- pretalx_slack/apps.py
+++ pretalx_slack/apps.py
@@ -3,17 +3,17 @@ from django.utils.translation import gettext_lazy
 
 
 class PluginApp(AppConfig):
-    name = 'pretalx_slack'
-    verbose_name = 'Slack integration for pretalx'
+    name = "pretalx_slack"
+    verbose_name = "Slack integration for pretalx"
 
     class PretalxPluginMeta:
-        name = gettext_lazy('Slack integration for pretalx')
-        author = 'Tobias Kunze'
+        name = gettext_lazy("Slack integration for pretalx")
+        author = "Tobias Kunze"
         description = gettext_lazy(
-            'Receive notifications whenever a submission changes its state.'
+            "Receive notifications whenever a submission changes its state."
         )
         visible = True
-        version = '0.0.0'
+        version = "0.0.0"
 
     def ready(self):
         from . import signals  # NOQA
--- pretalx_slack/signals.py
+++ pretalx_slack/signals.py
@@ -6,18 +6,18 @@ from django.dispatch import receiver
 from django.utils.translation import gettext_lazy as _
 from pretalx.submission.signals import submission_state_change
 
-webhook = settings.CONFIG.get('slack', 'webhook_url')
+webhook = settings.CONFIG.get("slack", "webhook_url")
 
 
-@receiver(submission_state_change, dispatch_uid='pretalx_slack')
+@receiver(submission_state_change, dispatch_uid="pretalx_slack")
 def submission_state_change(sender, old_state, submission, user, **kwargs):
     if not webhook:
-        print('no webhook url available')
+        print("no webhook url available")
         return
 
     try:
         text = _(
-            'Submission <{url}|»{title}«> has been changed from {old_state} to {new_state} by {name}.'
+            "Submission <{url}|»{title}«> has been changed from {old_state} to {new_state} by {name}."
         ).format(
             url=submission.orga_urls.base.full(),
             title=submission.title,
@@ -27,8 +27,8 @@ def submission_state_change(sender, old_state, submission, user, **kwargs):
         )
         requests.post(
             webhook,
-            headers={'Content-type': 'application/json'},
-            data=json.dumps({'text': text}),
+            headers={"Content-type": "application/json"},
+            data=json.dumps({"text": text}),
         )
     except Exception as e:
         print(e)
